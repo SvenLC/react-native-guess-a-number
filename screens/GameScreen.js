ddf38df572be5e7846531c45bf0e12e0cfc1cbbb
Sven Le Cann <sven.lecann@live.fr>
2020-12-05 01:46:18 +0100

    Section 4 - 69

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -19,13 +19,16 @@ const GameScreen = (props) => {
   const [currentGuess, setCurrentGuess] = useState(
     generateRandomBetween(1, 100, props.userChoice)
   );
+  const [rounds, setRounds] = useState(0);
   const currentLow = useRef(1);
   const currentHigh = useRef(100);
 
+  const { userChoice, onGameOver } = props;
   useEffect(() => {
-    if (currentGuess === props.userChoice) {
+    if (currentGuess === userChoice) {
+      onGameOver(rounds);
     }
-  });
+  }, [currentGuess, userChoice, onGameOver]);
 
   const nextGuessHandler = (direction) => {
     if (
@@ -48,6 +51,7 @@ const GameScreen = (props) => {
       currentGuess
     );
     setCurrentGuess(nextNumber);
+    setRounds((curRounds) => curRounds + 1);
   };
 
   return (
