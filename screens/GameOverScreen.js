0fdc09ee847a79f52256ea0f887f03d08fde96cf
Sven Le Cann <sven.lecann@live.fr>
2020-12-07 10:53:33 +0100

    Section 4 - 76

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -8,7 +8,12 @@ const GameOverScreen = (props) => {
       <TitleText>The Game is Over!</TitleText>
       <View style={styles.imageContainer}>
         <Image
+          // fadeDuration={100}
           source={require('../assets/success.png')}
+          // source={{
+          //   uri:
+          //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg',
+          // }}
           style={styles.images}
           resizeMode='cover'
         />
