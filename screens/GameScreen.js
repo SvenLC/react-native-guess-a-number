b264c9acfdb1158b3612af44452f9bfac64c927d
Sven Le Cann <sven.lecann@live.fr>
2020-12-08 12:03:12 +0100

    Section 5 - 98

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -129,6 +129,32 @@ const GameScreen = (props) => {
       </View>
     );
   }
+
+  return (
+    <View style={styles.screen}>
+      <Text style={DefaultStyles.title}>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <MainButton onPress={nextGuessHandler.bind(this, 'lower')}>
+          <Ionicons name='md-remove' size={24} color='white' />
+        </MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, 'greater')}>
+          <Ionicons name='md-add' size={24} color='white' />
+        </MainButton>
+      </Card>
+      <View style={listContainerStyle}>
+        {/* <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) => renderListItem(guess, pastGuesses.length - index))}
+        </ScrollView> */}
+        <FlatList
+          keyExtractor={(item) => item}
+          data={pastGuesses}
+          renderItem={renderListItem.bind(this, pastGuesses.length)}
+          contentContainerStyle={styles.list}
+        />
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
