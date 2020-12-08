d45b6237b20c9905e5ef9bd361290da90cc63f8f
Sven Le Cann <sven.lecann@live.fr>
2020-12-08 10:07:01 +0100

    Section 5 - 94

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,12 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import BodyText from '../components/BodyText';
 import MainButton from '../components/MainButton';
 import TitleText from '../components/TitleText';
@@ -7,31 +14,33 @@ import Colors from '../constants/colors';
 
 const GameOverScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <TitleText>The Game is Over!</TitleText>
-      <View style={styles.imageContainer}>
-        <Image
-          // fadeDuration={100}
-          source={require('../assets/success.png')}
-          // source={{
-          //   uri:
-          //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg',
-          // }}
-          style={styles.images}
-          resizeMode='cover'
-        />
-      </View>
-      <View style={styles.resultContainer}>
-        <BodyText style={styles.resultText}>
-          Your phone needed{' '}
-          <Text style={styles.hightlight}>{props.roundsNumber}</Text> rounds to
-          guess the number{' '}
-          <Text style={styles.hightlight}>{props.userNumber}</Text>
-        </BodyText>
-      </View>
+    <ScrollView>
+      <View style={styles.screen}>
+        <TitleText>The Game is Over!</TitleText>
+        <View style={styles.imageContainer}>
+          <Image
+            // fadeDuration={100}
+            source={require('../assets/success.png')}
+            // source={{
+            //   uri:
+            //     'https://cdn.pixabay.com/photo/2016/05/05/23/52/mountain-summit-1375015_960_720.jpg',
+            // }}
+            style={styles.images}
+            resizeMode='cover'
+          />
+        </View>
+        <View style={styles.resultContainer}>
+          <BodyText style={styles.resultText}>
+            Your phone needed{' '}
+            <Text style={styles.hightlight}>{props.roundsNumber}</Text> rounds
+            to guess the number{' '}
+            <Text style={styles.hightlight}>{props.userNumber}</Text>
+          </BodyText>
+        </View>
 
-      <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
-    </View>
+        <MainButton onPress={props.onRestart}>NEW GAME</MainButton>
+      </View>
+    </ScrollView>
   );
 };
 
@@ -42,13 +51,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   imageContainer: {
-    width: 300,
-    height: 300,
-    borderRadius: 150,
+    width: Dimensions.get('window').width * 0.7,
+    height: Dimensions.get('window').width * 0.7,
+    borderRadius: (Dimensions.get('window').width * 0.7) / 2,
     borderWidth: 3,
     borderColor: 'black',
     overflow: 'hidden',
-    marginVertical: 30,
+    marginVertical: Dimensions.get('window').height / 30,
   },
   images: {
     width: '100%',
@@ -60,11 +69,11 @@ const styles = StyleSheet.create({
   },
   resultContainer: {
     marginHorizontal: 30,
-    marginVertical: 15,
+    marginVertical: Dimensions.get('window').height / 60,
   },
   resultText: {
     textAlign: 'center',
-    fontSize: 20,
+    fontSize: Dimensions.get('window').height < 400 ? 16 : 20,
   },
 });
 
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -7,6 +7,7 @@ import {
   Text,
   View,
   FlatList,
+  Dimensions,
 } from 'react-native';
 import Card from '../components/Card';
 import MainButton from '../components/MainButton';
@@ -74,6 +75,11 @@ const GameScreen = (props) => {
     ]);
   };
 
+  let listContainerStyle = styles.listContainer;
+  if (Dimensions.get('window').width < 350) {
+    listContainerStyle = styles.listBigContainer;
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={DefaultStyles.title}>Opponent's Guess</Text>
@@ -112,13 +118,17 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginTop: 20,
+    marginTop: Dimensions.get('window').height > 600 ? 20 : 5,
     width: 400,
     maxWidth: '90%',
   },
   listContainer: {
     flex: 1,
     width: '60%',
+  },
+  listBigContainer: {
+    flex: 1,
+    width: '80%',
   },
   list: {
     flexGrow: 1,
